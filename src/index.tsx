33cbca2cbc900dd6ba6483198de7da2b058d4cd2
Matt Gleich <git@mattglei.ch>
2022-08-23 17:43:16 -0400

    feat: create note
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/index.tsx
+++ src/index.tsx
@@ -1,40 +1,45 @@
-import { Form, ActionPanel, Action, showToast } from "@raycast/api";
+import { Form, ActionPanel, Action, showToast, getPreferenceValues } from "@raycast/api";
+import path from "path";
+import fetch from "node-fetch";
 
 type Values = {
-  textfield: string;
-  textarea: string;
-  datepicker: Date;
-  checkbox: boolean;
-  dropdown: string;
-  tokeneditor: string[];
+  name: string;
+  folder: string;
+  class: string;
 };
 
 export default function Command() {
-  function handleSubmit(values: Values) {
-    console.log(values);
-    showToast({ title: "Submitted form", message: "See logs for submitted values" });
+  const classes = ["CSCI 140", "PHIL 101", "MEDG 101"];
+  const loading = false;
+  const classDropdownItems = classes.map((c) => <Form.Dropdown.Item value={c} title={c} />);
+  async function handleSubmit(values: Values) {
+    showToast({ title: "Creating document", message: "Starting" });
+    const data = await fetch("https://neptune.api.mattglei.ch/note", {
+      method: "POST",
+      headers: { Authorization: "Bearer " + getPreferenceValues().token },
+      body: JSON.stringify({
+        name: values.name,
+        folder: path.join("College", values.class, values.folder),
+      }),
+    });
+    console.log(data);
+    showToast({ title: "Created document", message: "Done" });
   }
 
   return (
     <Form
+      isLoading={loading}
       actions={
         <ActionPanel>
           <Action.SubmitForm onSubmit={handleSubmit} />
         </ActionPanel>
       }
     >
-      <Form.Description text="This form showcases all available form elements." />
-      <Form.TextField id="textfield" title="Text field" placeholder="Enter text" defaultValue="Raycast" />
-      <Form.TextArea id="textarea" title="Text area" placeholder="Enter multi-line text" />
-      <Form.Separator />
-      <Form.DatePicker id="datepicker" title="Date picker" />
-      <Form.Checkbox id="checkbox" title="Checkbox" label="Checkbox Label" storeValue />
-      <Form.Dropdown id="dropdown" title="Dropdown">
-        <Form.Dropdown.Item value="dropdown-item" title="Dropdown Item" />
+      <Form.TextField id="name" title="Name" placeholder="Name" />
+      <Form.TextField id="folder" title="Folder" placeholder="Folder" />
+      <Form.Dropdown id="class" title="Class">
+        {...classDropdownItems}
       </Form.Dropdown>
-      <Form.TagPicker id="tokeneditor" title="Tag picker">
-        <Form.TagPicker.Item value="tagpicker-item" title="Tag Picker Item" />
-      </Form.TagPicker>
     </Form>
   );
 }
